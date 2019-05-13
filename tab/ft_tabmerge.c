88ace00fa3a8a35010743882501d9d28eecf3017
Administrator <admin@example.com>
2019-05-13 13:51:32 +0200

    corrected some bugs

--- tab/ft_tabmerge.c
+++ tab/ft_tabmerge.c
@@ -6,7 +6,7 @@
 /*   By: maboye <maboye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 13:27:44 by maboye            #+#    #+#             */
-/*   Updated: 2019/05/13 13:49:07 by maboye           ###   ########.fr       */
+/*   Updated: 2019/05/13 13:51:27 by maboye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ char			**ft_tabmerge(const char **t1, const char **t2)
 		return (t1 ? ft_tabdup(t1) : ft_tabdup(t2));
 	else if (!t1 && !t2)
 		return (NULL);
-	i = 0;
-	j = 0;
+	i = -1;
+	j = -1;
 	if (!(ret = (char **)malloc(sizeof(char *)
 			* (ft_tablen((void **)t1) + ft_tablen((void **)t2) + 1))))
 		return (NULL);
-	while (t1 && t1[i])
-		ret[i] = ft_strdup(t1[i++]);
-	while (t2 && t2[j])
-		ret[i++] = ft_strdup(t2[j++]);
+	while (t1 && t1[++i])
+		ret[i] = ft_strdup(t1[i]);
+	while (t2 && t2[++j])
+		ret[i++] = ft_strdup(t2[j]);
 	ret[i] = NULL;
 	return (ret);
 }
