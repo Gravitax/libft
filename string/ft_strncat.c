26e04c44cec01d4be794f012ab279d6cdba101d1
Administrator <admin@example.com>
2020-08-10 20:46:51 +0200

    libft

--- string/ft_strncat.c
+++ string/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: maboye <maboye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 11:46:37 by maboye            #+#    #+#             */
-/*   Updated: 2020/08/10 20:35:44 by maboye           ###   ########.fr       */
+/*   Updated: 2020/08/10 20:45:42 by maboye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ char			*strncat(char *dst, const char *src, size_t n)
 	int	len1;
 	int	len2;
 
-	len1 = ft_strlen(dest);
+	len1 = ft_strlen(dst);
 	len2 = ft_strlen(src);
 	if (len2 < (int)n)
 		ft_strcpy(&dst[len1], src);
 	else
 		ft_strncpy(&dst[len1], src, n);
-	dest[len1 + n] = '\0';
-	return (dest);
+	dst[len1 + n] = '\0';
+	return (dst);
 }
